f0edaa2dbffa3dffcc69cb9db408d57849add103
Konab <konab-job@yandex.ru>
2018-10-20 12:54:33 +0300

    add app

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,6 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-	test_data = 'Hellp world!'
+	test_data = 'Hello world!'
 	# test_data = Employee.query.get(1) 
 	return render_template('index.html', data=test_data)
